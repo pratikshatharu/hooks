0aa724ee2c58905e92d962d8002a11d3cee9c020
PRATIKSHA01 <pratikshachaudhary728@gmail.com>
2021-06-09 10:31:35 +0545

    setFullName

--- reacthooks/src/components/hooks/Forms.jsx
+++ reacthooks/src/components/hooks/Forms.jsx
@@ -2,11 +2,23 @@ import React, { useState } from 'react'
 
 
 const Form = () => {
+    const [name, setName] = useState('');
+    const [fullName, setFullName] = useState();
+    let value
+
+    const InputEvent = (event) => {
+        console.log(event.target.value);
+        setName(event.target.value)
+        value = { name }
+    }
+    const onSubmit = () => {
+        setFullName(name);
+    }
     return (
         <>
-            <h1>hello</h1>
-            <input type='text' placeholder='ENTER YOUR NAME'></input>
-            <button>click me</button>
+            <h1>hello {fullName}</h1>
+            <input type='text' placeholder='ENTER YOUR NAME' onChange={InputEvent}></input>
+            <button onClick={onSubmit}>click me</button>
         </>
 
     );
